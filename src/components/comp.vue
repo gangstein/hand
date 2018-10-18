e9c1223f99822f29b6d86727e66477ce566bf6c4
gangstein <gangstein@gmail.com>
2018-10-19 00:50:38 +0300

    new

--- src/components/comp.vue
+++ src/components/comp.vue
@@ -11,28 +11,25 @@
 			};
 		},
 		mounted() {
-			this.drawMap();
+			ymaps.ready(this.drawMap)
+			  .then((r) => console.log(r))
+			  .catch(e => console.log(e))
 		},
 		methods: {
 			drawMap() {
-				ymaps.ready(init);
-
-				function init() {
-					let myMap = new ymaps.Map("map", {
-						center: [51.661535, 39.200287],
-						zoom:   14
-					});
-					myMap.behaviors.disable(["drag", "rightMouseButtonMagnifier", "ruler"]);
-					var myPlacemark = new ymaps.Placemark([51.661535, 39.200287], {}, {
-						iconLayout:      "default#image",
-						iconImageHref:   "group-2.svg",
-						iconImageSize:   [30, 42],
-						iconImageOffset: [-3, -42]
-					});
-					myMap.geoObjects.add(myPlacemark);
-					this.myMap = myMap;
-				}
-
+				this.myMap = new ymaps.Map("map", {
+					center: [51.661535, 39.200287],
+					zoom:   14,
+					controls: [],
+				});
+				this.myMap.behaviors.disable(["drag", "rightMouseButtonMagnifier", "scrollZoom"]);
+				const myPlacemark = new ymaps.Placemark([51.661535, 39.200287], {}, {
+					iconLayout:      "default#image",
+					iconImageHref:   "group-2.svg",
+					iconImageSize:   [30, 42],
+					iconImageOffset: [-3, -42]
+				});
+				this.myMap.geoObjects.add(myPlacemark);
 			}
 		}
 	};
