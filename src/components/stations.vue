54dbc85cc9b0a9fd3c2634b107fc530ade9f7cc8
gangstein <gangstein@gmail.com>
2018-10-22 00:15:19 +0300

    scrollbar

--- src/components/stations.vue
+++ src/components/stations.vue
@@ -1,60 +1,51 @@
 <template>
-	<div class="baron baron__root baron__clipper _MODIFIER" ref="baron__root">
-		<div class="baron__scroller" ref="baron__scroller">
-			<div v-for="st in getServerDataStations" :id="st.id">
-				<p>{{st.name.split("»")[0]}}»</p>
-				<p>{{st.name.split("»")[1]}}</p>
-			</div>
-		</div>
-		<div class="baron__track" ref="baron__track">
-			<div class="baron__free">
-				<div class="baron__bar" ref="baron__bar"></div>
-			</div>
+	<div class="root" ref="root">
+		<div class="scroller">
+			<perfect-scrollbar>
+				<div v-for="st in getServerDataStations" :id="st.id">
+					<p>{{st.name.split("»")[0]}}»</p>
+					<p>{{st.name.split("»")[1]}}</p>
+				</div>
+			</perfect-scrollbar>
 		</div>
 	</div>
 </template>
 <script>
-	import { mapGetters } from "vuex";
-	import baron          from "baron";
+	import { mapGetters }       from "vuex";
+	import { PerfectScrollbar } from "vue2-perfect-scrollbar";
+	import "../../public/styles/vue2-perfect-scrollbar.css";
 
 	export default {
-		name:     "stations",
-		computed: {
+		name:       "stations",
+		computed:   {
 			...mapGetters(["getServerDataStations"])
 		},
-		mounted() {
-			baron(
-			  {
-				  root:           this.$refs.baron__root,
-				  scroller:       this.$refs.baron__scroller,
-				  bar:            this.$refs.baron__bar,
-				  track:          this.$refs.baron__track,
-				  barOnCls:       "_scrollbar",
-				  scrollingCls:   "_scrolling",
-				  draggingCls:    "_dragging",
-				  direction:      "h",
-				  resizeDebounce: .2,
-				  rtl:            true
-			  }
-			);
-			// console.log(this.$refs.container.scrollHeight);
+		components: {
+			PerfectScrollbar
 		},
-		methods:  {}
+		mounted() {
+		},
+		methods:    {}
 	};
 </script>
 <style scoped lang="scss">
-	.baron__root {
-		margin-left: 50px;
+	.root {
 		width: 80%;
 		height: 80%;
+		margin-left: 50px;
 		font-size: 12px;
-		position: relative;
-		
-		div {
-			margin-bottom: 20px;
-			p {
-				margin: 0;
-			}
+		overflow: hidden;
+		.scroller {
+			height: 100%;
+		}
+	}
+	.ps {
+		height: 100%;
+	}
+	div {
+		margin-bottom: 20px;
+		p {
+			margin: 0;
 		}
 	}
 </style>
